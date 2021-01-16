7afb5922041a52db06d88fad343936431b74c871
Erin Untermeyer <aerinuntermeyer@gmail.com>
2021-01-15 20:18:38 -0700

    Finish Bob challenge

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,8 +1,30 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const isYelling = (string) => {
+	return string === string.toUpperCase()
+}
+
+const isQuestion = (string) => {
+	return string.slice(-1) === '?'
+}
+
+const noLetters = (string) => {
+	return /^[^a-zA-Z]*$/.test(string)
+}
 
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
+	message = message.replace(/\s/g, '')
+	if (noLetters(message) && isQuestion(message)) {
+		return 'Sure.'
+	} else if (noLetters(message) && message === "") {
+		return 'Fine. Be that way!'
+	} else if (noLetters(message)) {
+		return 'Whatever.'
+	} else if (isQuestion(message) && isYelling(message)) {
+		return 'Calm down, I know what I\'m doing!'
+	} else if (isYelling(message)) {
+		return 'Whoa, chill out!'
+	} else if (isQuestion(message)) {
+		return 'Sure.'
+	} else {
+		return 'Whatever.'
+	}
 };
